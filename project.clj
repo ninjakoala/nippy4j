8512514f68a9fd99e5dbd3ee3aeea4177d2b0305
Neil Prosser <neil.prosser@gmail.com>
2016-05-21 12:37:07 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.ninjakoala/nippy4j "1.0.0-SNAPSHOT"
   :description "Java wrapper for Nippy"
-  :dependencies [[com.taoensso/nippy "2.10.0"]
+  :dependencies [[com.taoensso/nippy "2.11.1"]
                  [org.clojure/clojure "1.7.0"]]
   :exclusions [org.clojure/clojure]
   :profiles {:dev {:dependencies [[com.taoensso/faraday "1.8.0"]]
