1bbafab1750b59949e8bc678c6569a5a05a5bf97
Neil Prosser <neil.prosser@gmail.com>
2017-06-16 21:17:29 +0100

    Update project configuration

--- project.clj
+++ project.clj
@@ -1,10 +1,20 @@
 (defproject com.ninjakoala/nippy4j "1.0.0-SNAPSHOT"
   :description "Java wrapper for Nippy"
-  :dependencies [[com.taoensso/nippy "2.11.1"]
-                 [org.clojure/clojure "1.7.0"]]
-  :exclusions [org.clojure/clojure]
-  :profiles {:dev {:dependencies [[com.taoensso/faraday "1.8.0"]]
-                   :plugins [[lein-release "1.0.5"]]}}
+  :url "https://github.com/ninjakoala/nippy4j"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[com.taoensso/nippy "2.13.0"]
+                 [org.clojure/clojure "1.8.0"]]
+  :profiles {:dev {:dependencies [[com.taoensso/faraday "1.9.0"]
+                                  [midje "1.8.3"]]
+                   :plugins [[lein-midje "3.2.1"]]}}
+  :aliases {"test" ["midje"]}
   :aot [ninjakoala.nippy4j]
-  :lein-release {:deploy-via :clojars
-                 :clojars-url "clojars@clojars.brislabs.com:"})
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "--no-sign"]
+                  ["deploy" "clojars"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]])
